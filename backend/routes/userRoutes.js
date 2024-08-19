403b46ad12f3f17198af25a5bdbe1ebd2aa0406e
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-19 15:23:50 -0500

    :bug: delete route "createUser"

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import {getUserProfile, createUser, updateUserProfile, deleteUser, getAllUsers} from '../controllers/userController.js';
+import {getUserProfile, updateUserProfile, deleteUser, getAllUsers} from '../controllers/userController.js';
 
 const router = express.Router();
 
-router.get('/:id', getUserProfile);        // Obtener el perfil de un usuario por ID
-router.post('/', createUser);              // Crear un nuevo usuario
+router.get('/:id', getUserProfile);        // Obtener el perfil de un usuario por ID // Crear un nuevo usuario
 router.put('/:id', updateUserProfile);     // Actualizar el perfil de un usuario
 router.delete('/:id', deleteUser);         // Eliminar un usuario por ID
 router.get('/', getAllUsers);              // Obtener todos los usuarios
