c4e7f79e104e43e4e705ddc970a94af8add7d2cb
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-19 15:21:51 -0500

    :bug: delete createuser function, because it´s unnecessary

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,32 +14,6 @@ export const getUserProfile = async (req, res) => {
   }
 };
 
-export const createUser = async (req, res) => {
-  try {
-    const {nombre, email, password} = req.body;
-    
-    // Verificar si el usuario ya existe
-    const existingUser = await User.findOne({where: {email}});
-    if (existingUser) {
-      return res.status(400).json({error: 'User already exists with this email'});
-    }
-    
-    // Crear nuevo usuario con la contraseña encriptada
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    
-    const newUser = await User.create({
-      nombre,
-      email,
-      hashed_password: hashedPassword,
-    });
-    
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(500).json({error: error.message});
-  }
-};
-
 export const updateUserProfile = async (req, res) => {
   try {
     const {id} = req.params;
