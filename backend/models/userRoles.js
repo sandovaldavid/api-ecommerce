ffc51fb32ef4945e78eb0a91cb736215b89bdea1
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-21 22:24:20 -0500

    :card_file_box: new table intermediate userRoles

---
+++ backend/models/userRoles.js
@@ -0,0 +1,6 @@
+import User from "./user.js";
+import Roles from "./roles.js";
+
+// Relación muchos a muchos entre User y Roles
+User.belongsToMany(Roles, {through: 'UserRoles'});
+Roles.belongsToMany(User, {through: 'UserRoles'});
