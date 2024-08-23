78bb993474d3adea26288ee48d6553ec58ab7f32
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-23 18:25:11 -0500

    :art: enhancement method register

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,17 +1,21 @@
 import jwt from "jsonwebtoken";
-import { Roles, User } from "../models/userRoles.js";
+import { User } from "../models/userRoles.js";
 import config from "../config/config.js";
 
-// Metodo de registro de Comentarios
+//TODO: Implementar el método de registro de comentarios
+// Método de registro de Comentarios
 export const register = async (req, res) => {
   try {
-    const { firstName, secondName, lastName_father: lastNameFather, lastName_mother: lastNameMother, email, password, roles } = req.body;
+    const {
+      firstName,
+      secondName,
+      lastName_father: lastNameFather,
+      lastName_mother: lastNameMother,
+      email,
+      password,
+      roles
+    } = req.body;
     
-    // Verificar si el usuario ya existe
-    const existingUser = await User.findOne({ where: { email } });
-    if (existingUser) {
-      return res.status(400).json({ error: "User already exists" });
-    }
     // Crear el usuario con la contraseña encriptada
     const newUser = await User.create({
       firstName,
@@ -21,30 +25,7 @@ export const register = async (req, res) => {
       email,
       hashed_password: password
     });
-    
-    // Verificación de roles
-    if (roles) {
-      // Buscar los roles que coinciden con los nombres proporcionados en el array `roles`
-      const foundRoles = await Roles.findAll({
-        where: {
-          name: roles // Esto equivale a `$in` en MongoDB
-        }
-      });
-      
-      // Asociar los roles encontrados al usuario usando la tabla intermedia
-      await newUser.addRole(foundRoles);
-      /*addRoles es un metodo generado por Sequelize para la tabla intermedia muchos a muchos*/
-    } else {
-      // Si no se especifican roles, asignar el rol predeterminado "user"
-      const defaultRole = await Roles.findOne({
-        where: {
-          name: "user"
-        }
-      });
-      
-      // Asociar el rol predeterminado al usuario usando la tabla intermedia
-      await newUser.addRole(defaultRole); /*addRoles es un metodo generado por Sequelize */
-    }
+    await newUser.addRole(roles);
     
     // Generar el token JWT
     const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, { expiresIn: "1h" });
@@ -60,12 +41,13 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
+    //TODO: Mejorar la lectura del código, coloca las validaciones en el archivo verifySignUp.js
     // Buscar al usuario por correo electrónico
     const user = await User.findOne({ where: { email } });
     if (!user) {
       return res.status(401).json({ error: "Invalid email or password" });
     }
-    
+    //TODO: Mejorar la lectura del código, coloca las validaciones en el archivo verifySignUp.js
     // Verificar la contraseña usando bcrypt
     const isPasswordValid = await user.comparePassword(password, user.hashed_password);
     if (!isPasswordValid) {
