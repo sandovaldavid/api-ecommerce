aed76a5fabd1cc89176c8ab7223eec857f7f4c46
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-23 18:52:45 -0500

    :safety_vest: add vefifytoken validation

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,7 +5,7 @@ import { verifySignUp, authJwt } from "../middlewares/index.js";
 const router = express.Router();
 
 // noinspection JSCheckFunctionSignatures
-router.post("/register", authJwt.isAdmin, [verifySignUp.checkDuplicateEmail, verifySignUp.checkRolesExisted], register);
+router.post("/register", [authJwt.verifyToken, authJwt.isAdmin], [verifySignUp.checkDuplicateEmail, verifySignUp.checkRolesExisted], register);
 router.post("/login", login);
 
 export default router;  // Exportar el router como default
