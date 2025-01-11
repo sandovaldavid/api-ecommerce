37a9dab6b57fe660992c01db7fb5ff371999c510
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-11 16:01:56 -0500

    ✨ Refactor error handling in verifySignIn middleware to improve user feedback and add try-catch for better error management

--- backend/middlewares/verifySignIn.js
+++ backend/middlewares/verifySignIn.js
@@ -1,17 +1,22 @@
 import { User } from "../models/userRoles.js";
 
 export const checkEmailAndPassword = async (req, res, next) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ where: { email } });
-    const isPasswordValid = await user.comparePassword(password, user.hashed_password);
-    req.body.userId = user.id;
-  
-    if (!user) {
-        return res.status(401).json({ error: "Invalid email or password" });
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ where: { email } });
+
+        if (!user) {
+            return res.status(401).json({ error: "Invalid email or password" });
+        }
+
+        const isPasswordValid = await user.comparePassword(password, user.hashed_password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: "Invalid email or password" });
+        }
+
+        req.body.userId = user.id;
+        next();
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
     }
-  
-    if (!isPasswordValid) {
-        return res.status(401).json({ error: "Invalid email or password" });
-    }
-    next();
 };
