cffa5172cc3f33935eccb2b74efe7d4ee09ef543
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-11 16:25:48 -0500

    ✨ Allow indentation tabs in ESLint configuration for improved flexibility

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -16,7 +16,7 @@ export default [
             "array-bracket-spacing": ["error", "never"],
             "computed-property-spacing": ["error", "never"],
             "space-before-function-paren": ["error", "always"], // Ajuste similar a WebStorm
-            "no-tabs": ["error", { allowIndentationTabs: false }], // No permitir tabs para indentación
+            "no-tabs": ["error", { allowIndentationTabs: true }], // No permitir tabs para indentación
             "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
             "object-shorthand": ["error", "always"],
             "no-trailing-spaces": ["error", { skipBlankLines: true }],
