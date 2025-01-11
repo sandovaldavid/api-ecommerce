19018a92ed4d859082b291f749f85fa9504f5fda
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-11 16:02:08 -0500

    ✨ Remove redundant verification middleware from getAllUsers route for cleaner code

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { getUserProfile, updateUserProfile, deleteUser, getAllUsers } from "../controllers/userController.js";
-import { authJwt, verifySignUp } from "../middlewares/index.js";
+import { authJwt } from "../middlewares/index.js";
 
 const router = express.Router();
 
 router.get("/:id", getUserProfile);        // Obtener el perfil de un usuario por ID // Crear un nuevo usuario
 router.put("/:id", updateUserProfile);     // Actualizar el perfil de un usuario
 router.delete("/:id", deleteUser);         // Eliminar un usuario por ID
-router.get("/", [authJwt.verifyToken, authJwt.isAdmin], [verifySignUp.checkDuplicateEmail, verifySignUp.checkRolesExisted], getAllUsers);              // Obtener todos los usuarios
+router.get("/", [authJwt.verifyToken, authJwt.isAdmin], getAllUsers); // Obtener todos los usuarios
 
 export default router;
