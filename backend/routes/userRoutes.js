8fd18985d6f1a80b35a69c4f557a956931a7dad9
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-13 16:01:28 -0500

    ✨ Add owner or admin access control to user profile routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,8 +6,8 @@ const router = express.Router();
 
 // Protected routes (require authentication)
 router.use(authJwt.verifyToken);                    // Middleware para verificar el token de autenticación
-router.get("/profile/:id", getUserProfile);         // Obtener el perfil de un usuario por ID
-router.put("/profile/:id", updateUserProfile);      // Actualizar el perfil de un usuario
+router.get("/profile/:id",authJwt.isOwnerOrAdmin("id") ,getUserProfile);         // Obtener el perfil de un usuario por ID
+router.put("/profile/:id", authJwt.isOwnerOrAdmin("id"), updateUserProfile);      // Actualizar el perfil de un usuario
 
 // Admin routes (require admin role)
 router.use(authJwt.hasRoles("admin"));          // Middleware para verificar el rol de administrador
