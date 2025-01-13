d267d2ec76d801185b04e80576ebf1879823671b
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-13 15:11:14 -0500

    ✨ Add getReviews endpoint and apply token verification middleware to review routes

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createReview } from "../controllers/reviewController.js";
+import { createReview, getReviews } from "../controllers/reviewController.js";
 import { authJwt } from "../middlewares/index.js";
 
 const router = express.Router();
 
+router.use(authJwt.verifyToken);
 router.post("/", [authJwt.verifyToken, authJwt.hasRoles("user", "admin", "moderator")], createReview);
+router.get("/", getReviews);
 
 export default router;
