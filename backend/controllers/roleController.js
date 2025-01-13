37cd46b57af5d5d4ef86bec133c3c2140230b734
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-13 11:46:16 -0500

    ✨ Add role management functions: deleteRole, assignRole, and removeRole

--- backend/controllers/roleController.js
+++ backend/controllers/roleController.js
@@ -20,3 +20,52 @@ export const getAllRoles = async (req, res) => {
         res.status(500).json({ error: e.message });
     }
 };
+
+export const deleteRole = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const role = await Roles.findByPk(id);
+        if (!role) {
+            return res.status(404).json({ error: "Role not found" });
+        }
+        await role.destroy();
+        res.status(204).send();
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+export const assignRole = async (req, res) => {
+    try {
+        const { userId, roleId } = req.body;
+        const user = await User.findByPk(userId);
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        const role = await Roles.findByPk(roleId);
+        if (!role) {
+            return res.status(404).json({ error: "Role not found" });
+        }
+        await user.addRole(role);
+        res.status(200).send();
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const removeRole = async (req, res) => {
+    try {
+        const { userId, roleId } = req.body;
+        const user = await User.findByPk(userId);
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        const role = await Roles.findByPk(roleId);
+        if (!role) {
+            return res.status(404).json({ error: "Role not found" });
+        }
+        await user.removeRole(role);
+        res.status(200).send();
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
