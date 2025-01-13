74cfe8991e22d16f2f42b912c89fc5ae8835e964
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-13 11:46:10 -0500

    ✨ Refactor role routes to improve code readability with comments

--- backend/routes/roleRoutes.js
+++ backend/routes/roleRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { createRole,getAllRoles } from "../controllers/roleController.js";
+import { createRole, getAllRoles,  } from "../controllers/roleController.js";
 
 const router = express.Router();
 
-router.post("/", createRole);
-router.get("/", getAllRoles);
+router.post("/", createRole);                   // Crear un nuevo rol
+router.get("/", getAllRoles);                   // Obtener todos los roles
 
 export default router;
