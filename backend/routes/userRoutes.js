2028ea863c2b8f04f07d7f93976b5b89f2925aa8
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-13 10:36:44 -0500

    ✨ Remove redundant comment in userRoutes.js for clarity

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,7 +4,7 @@ import { authJwt } from "../middlewares/index.js";
 
 const router = express.Router();
 
-router.get("/:id", getUserProfile);        // Obtener el perfil de un usuario por ID // Crear un nuevo usuario
+router.get("/:id", getUserProfile);        // Obtener el perfil de un usuario por ID
 router.put("/:id", updateUserProfile);     // Actualizar el perfil de un usuario
 router.delete("/:id", deleteUser);         // Eliminar un usuario por ID
 router.get("/", [authJwt.verifyToken, authJwt.isAdmin], getAllUsers); // Obtener todos los usuarios
