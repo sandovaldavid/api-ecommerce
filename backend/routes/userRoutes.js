876c8c24b8885410f8b37cefa5b7ef3a47be618f
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-13 10:38:54 -0500

    ✨ Update user routes to include 'profile' in endpoint paths for clarity

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,8 +4,8 @@ import { authJwt } from "../middlewares/index.js";
 
 const router = express.Router();
 
-router.get("/:id", getUserProfile);        // Obtener el perfil de un usuario por ID
-router.put("/:id", updateUserProfile);     // Actualizar el perfil de un usuario
+router.get("/profile/:id", getUserProfile);        // Obtener el perfil de un usuario por ID
+router.put("/profile/:id", updateUserProfile);     // Actualizar el perfil de un usuario
 router.delete("/:id", deleteUser);         // Eliminar un usuario por ID
 router.get("/", [authJwt.verifyToken, authJwt.isAdmin], getAllUsers); // Obtener todos los usuarios
 
