f4e615f70f269946439e8d516ce2986004af78ba
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-13 21:23:00 -0500

    ✨ Enhance assignRole route with admin role enforcement for improved access control

--- backend/routes/roleRoutes.js
+++ backend/routes/roleRoutes.js
@@ -8,6 +8,6 @@ router.use(authJwt.verifyToken);
 router.post("/", createRole);                   // Crear un nuevo rol
 router.get("/", authJwt.hasRoles("user", "admin", "moderator"), getAllRoles);                   // Obtener todos los roles
 router.delete("/:id", authJwt.isAdmin , deleteRole);              // Eliminar un rol
-router.post("/assign", assignRole);             // Asignar un rol a un usuario
+router.post("/assign", authJwt.isAdmin, assignRole);             // Asignar un rol a un usuario
 router.post("/remove", removeRole);             // Eliminar un rol de un usuario
 export default router;
