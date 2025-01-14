91bef730aec969cbaed02a21a9704bfb1442808c
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-13 21:08:21 -0500

    ✨ Enhance roleRoutes with authentication middleware; enforce role-based access control for getAllRoles endpoint

--- backend/routes/roleRoutes.js
+++ backend/routes/roleRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { createRole, getAllRoles, deleteRole, assignRole, removeRole } from "../controllers/roleController.js";
+import { authJwt } from "../middlewares/index.js";
 
 const router = express.Router();
 
+router.use(authJwt.verifyToken);
 router.post("/", createRole);                   // Crear un nuevo rol
-router.get("/", getAllRoles);                   // Obtener todos los roles
+router.get("/", authJwt.hasRoles("user", "admin", "moderator"), getAllRoles);                   // Obtener todos los roles
 router.delete("/:id", deleteRole);              // Eliminar un rol
 router.post("/assign", assignRole);             // Asignar un rol a un usuario
 router.post("/remove", removeRole);             // Eliminar un rol de un usuario
