be06cea7c7ca38f4bc63f1de013898114864edb7
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-16 12:40:36 -0500

    ✨feat: add null field checks in registration route middleware

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,7 +5,7 @@ import { verifySignUp, authJwt,verifySignIn } from "../middlewares/index.js";
 const router = express.Router();
 
 // noinspection JSCheckFunctionSignatures
-router.post("/register", [authJwt.verifyToken, authJwt.isAdmin], [verifySignUp.checkDuplicateEmail, verifySignUp.checkRolesExisted], register);
+router.post("/register", [authJwt.verifyToken, authJwt.isAdmin], [verifySignUp.checkNullFields, verifySignUp.checkDuplicateEmail, verifySignUp.checkRolesExisted], register);
 router.post("/login",verifySignIn.checkEmailAndPassword, login);
 
 export default router;  // Exportar el router como default
