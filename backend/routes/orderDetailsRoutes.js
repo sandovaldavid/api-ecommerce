c4b07b713ed94e25b4f603dfeddabe926496a453
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-16 22:23:04 -0500

    ✨ feat: enhance orderDetailsRoutes with additional CRUD operations and authentication middleware

--- backend/routes/orderDetailsRoutes.js
+++ backend/routes/orderDetailsRoutes.js
@@ -1,9 +1,21 @@
 import express from "express";
-import { createOrderDetails, getOrderDetailsByOrderId } from "../controllers/orderDetailsController.js";
+import {
+    createOrderDetails,
+    getOrderDetailsByOrderId,
+    updateOrderDetails,
+    deleteOrderDetails
+} from "../controllers/orderDetailsController.js";
+import { authJwt } from "../middlewares/index.js";
 
 const router = express.Router();
 
+// Base middleware
+router.use(authJwt.verifyToken);
+
+// Routes
 router.post("/", createOrderDetails);
-router.get("/:orden_id", getOrderDetailsByOrderId);
+router.get("/order/:orderId", getOrderDetailsByOrderId);
+router.put("/:id", updateOrderDetails);
+router.delete("/:id", deleteOrderDetails);
 
 export default router;
