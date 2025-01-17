37bfcad7775b64047f3eb3625410f1a99fb0a9c1
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-16 21:50:44 -0500

    ✨ feat: update user profile routes to include admin access and adjust profile retrieval method

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { getUserProfile, updateUserProfile, deleteUser, getAllUsers } from "../controllers/userController.js";
+import { getUserProfileAdmin, updateUserProfile, deleteUser, getAllUsers, getUserProfile } from "../controllers/userController.js";
 import { authJwt } from "../middlewares/index.js";
 
 const router = express.Router();
 
 // Protected routes (require authentication)
 router.use(authJwt.verifyToken);                    // Middleware para verificar el token de autenticación
-router.get("/profile/:id", getUserProfile);         // Obtener el perfil de un usuario por ID
+router.get("/profile", getUserProfile);         // Obtener el perfil de un usuario por ID
 router.put("/profile/:id", updateUserProfile);      // Actualizar el perfil de un usuario
+router.put("/profile/:id", authJwt.isAdmin, getUserProfileAdmin);
 
 // Admin routes (require admin role)
 router.use(authJwt.hasRoles("admin"));          // Middleware para verificar el rol de administrador
