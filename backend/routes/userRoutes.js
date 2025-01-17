c2a537d482f1b59a84d155551e55baab4d3f4a3a
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-16 21:54:16 -0500

    ✨ feat: rename user profile retrieval method for consistency in userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,18 +1,18 @@
 import express from "express";
-import { getUserProfileAdmin, updateUserProfile, deleteUser, getAllUsers, getUserProfile } from "../controllers/userController.js";
+import { getUserProfile, updateUserProfile, deleteUser, getAllUsers, getProfile } from "../controllers/userController.js";
 import { authJwt } from "../middlewares/index.js";
 
 const router = express.Router();
 
 // Protected routes (require authentication)
 router.use(authJwt.verifyToken);                    // Middleware para verificar el token de autenticación
-router.get("/profile", getUserProfile);         // Obtener el perfil de un usuario por ID
+router.get("/profile", getProfile);         // Obtener el perfil de un usuario por ID
 
 // Admin routes (require admin role)
 router.use(authJwt.hasRoles("admin"));          // Middleware para verificar el rol de administrador
 router.get("/", getAllUsers);                   // Obtener todos los usuarios
 router.delete("/:id", deleteUser);              // Eliminar un usuario por ID
 router.put("/profile/:id", updateUserProfile);      // Actualizar el perfil de un usuario
-router.put("/profile/:id", getUserProfileAdmin);
+router.put("/profile/:id", getUserProfile);
 
 export default router;
