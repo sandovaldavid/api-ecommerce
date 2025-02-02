d388fd0dcc8822cef2c65e4a5e3b7fa4c1edc968
sandovaldavid <sandovaldavid2201@gmail.com>
2025-02-01 21:28:21 -0500

    ✨ feat: enhance database connection handling for production and development environments

--- models/index.js
+++ models/index.js
@@ -1,13 +1,38 @@
 import { Sequelize } from "sequelize";
 import config from "../config/config.js";
 
-export const sequelize = new Sequelize(
-    config.development.database,
-    config.development.username,
-    config.development.password,
-    {
-        host: config.development.host,
-        port: config.development.port,
-        dialect: config.development.dialect,
-    }
-);
+const env = process.env.NODE_ENV || "development";
+
+const sequelize = process.env.NODE_ENV === "production"
+    ? new Sequelize(config.production.url, {
+        ...config.production,
+        dialect: config[env].dialect,
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    })
+    : new Sequelize(
+        config[env].database,
+        config[env].username,
+        config[env].password,
+        {
+            host: config[env].host,
+            port: config[env].port,
+            dialect: config[env].dialect,
+            logging: console.log,
+            ...config[env]
+        }
+    );
+
+try {
+    await sequelize.authenticate();
+    console.log("Connection to database has been established successfully.");
+} catch (error) {
+    console.error("Unable to connect to the database:", error);
+}
+
+export { sequelize };
